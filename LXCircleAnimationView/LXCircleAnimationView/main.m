72e2414a14b0887d8a0cb04f8b907c5e2f84035b
lixin_1990@yeah.net <lixin_1990@yeah.net>
2017-06-09 13:57:47 +0800

    add missing file

---
+++ LXCircleAnimationView/LXCircleAnimationView/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  LXCircleAnimationView
+//
+//  Created by Leexin on 15/12/18.
+//  Copyright © 2015年 Garden.Lee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
